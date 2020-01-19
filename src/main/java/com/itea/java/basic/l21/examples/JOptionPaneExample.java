619a6a90918a7f80172eeb4661702d5128d48501
anton1113 <anton11131113@gmail.com>
2020-01-20 01:53:11 +0200

    JOptionPane

---
+++ src/main/java/com/itea/java/basic/l21/examples/JOptionPaneExample.java
@@ -0,0 +1,12 @@
+package com.itea.java.basic.l21.examples;
+
+import javax.swing.*;
+
+public class JOptionPaneExample {
+
+    public static void main(String[] args) {
+
+        String input = JOptionPane.showInputDialog("Please enter the number");
+        System.out.println(input);
+    }
+}
